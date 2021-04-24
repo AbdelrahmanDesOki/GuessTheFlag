6f6a66d9ade67aed419be813c9c475c22368840b
AbdelrahmanDesOki <desoki.abdelrahman@gmail.com>
2021-04-24 21:22:35 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Deso>k_ii on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
